d7acdcc444e90f1969c3b1dd0c5d473ed1af8cff
bladtcasper-afk <bladt.casper@gmail.com>
2026-09-17 17:51:52 +0200

    Removal of old and unused files

--- src/compas_singular/rhino/helpers/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Reading and writing the Rhino document: boundaries in, geometry out."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
