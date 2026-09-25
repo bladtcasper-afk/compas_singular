018f1a9bd451b6204b60a5c079fa23e0ce8f46a2
Casper Bladt <bladt.casper@gmail.com>
2026-09-25 12:00:11 +0200

    Delete compas_quad-main/compas_quad-main directory

--- compas_quad-main/compas_quad-main/AUTHORS.md
+++
@@ -1,3 +0,0 @@
-# Authors
-
-- Robin Oval <<robin.oval@princeton.edu>> [@robin-oval](https://github.com/robin-oval)
--- compas_quad-main/compas_quad-main/src/compas_quad/grammar/__init__.py
+++
@@ -1,5 +0,0 @@
-from .addition import *  # noqa F403
-from .deletion import *  # noqa F403
-from .lizard import *  # noqa F403
-
-__all__ = [name for name in dir() if not name.startswith('_')]
